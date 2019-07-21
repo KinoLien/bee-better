3316184d2da3ba528b896a3e2ff215d3d18ff6bf
KinoLien <miecowbai@gmail.com>
2019-07-21 12:54:21 +0800

    Add basic dashboard with daterange picker and chosen.

---
+++ assets/js/menu/dashboard.js
@@ -0,0 +1,31 @@
+
+var dateRangePicker = $("#daterangePicker");
+
+var maxDate = new Date();
+
+var initDateRange = [moment().subtract(3, 'days'), moment()];
+
+// for new daterange
+dateRangePicker.daterangepicker({
+	// minDate: minDate,
+	maxDate: maxDate,
+	startDate: initDateRange[0],
+	// endDate: maxDate,
+	timePicker: true,
+	timePicker24Hour: true,
+	timePickerSeconds: true,
+	locale: {
+		format: 'YYYY-MM-DD HH:mm:ss'
+		// cancelLabel: '取消',
+		// applyLabel: '確定',
+		// customRangeLabel: '自訂範圍'
+	},
+	ranges: {
+		'Nearly 3 Days': initDateRange,
+		'Nearly 7 Days': [moment().subtract(7, 'days'), moment()],
+		'Nearly 30 Days': [moment().subtract(30, 'days'), moment()]
+	},
+	alwaysShowCalendars: true
+});
+
+$(".chosen-select").chosen({width: '100%'});
