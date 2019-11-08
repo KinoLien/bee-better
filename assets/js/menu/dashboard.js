935d609ddeaf8769134fcea8ddb30c2e6a74e96b
KinoLien <miecowbai@gmail.com>
2019-11-08 16:56:33 +0800

    Refactoring the device line chart.

--- assets/js/menu/dashboard.js
+++ assets/js/menu/dashboard.js
@@ -2,15 +2,13 @@
 var dateRangePicker = $("#daterangePicker"),
 	deviceSelect = $("#chosen-device"),
 	propertiesSelect = $("#chosen-properties"),
-	flotLineChart = $("#flot-line-chart");
+	chartWrap = $(".flot-chart");
 
 var maxDate = new Date();
 
 var initDateRange = [moment().subtract(3, 'days'), moment()];
 
-var maxChartPoints = 500;
-
-var currentPlot = null;
+var flotLineChart = new DeviceChart({ appendTo: chartWrap });
 
 // for new daterange
 dateRangePicker.daterangepicker({
@@ -37,77 +35,8 @@ dateRangePicker.daterangepicker({
 
 $(".chosen-select").chosen({width: '100%'});
 
-var plotOptions = {
-	xaxis: {
-    	mode: 'time',
-    	timezone: "browser"
-    },
-    yaxis: {
-    	max: 100
-    },
-    legend: {
-        show: true
-    },
-    colors: ["#c62828", "#283593", "#00695C", "#F9A825", "#37474F", "#6A1B9A", "#0277BD", "#558B2F", "#EF6C00", "#4E342E"],
-    grid: {
-        color: "#999999",
-        clickable: true,
-        tickColor: "#D4D4D4",
-        borderWidth:0,
-        hoverable: true //IMPORTANT! this is needed for tooltip to work,
-    },
-    tooltip: true,
-    tooltipOpts: {
-        content: function(data, x, y, dataObject) {
-        	if ( currentPlot ) {
-        		var XdataIndex = dataObject.dataIndex;
-        		var currentAllData = currentPlot.getData();
-        		var outputs = [];
-        		currentAllData.forEach(function(series){
-        			outputs.push(series.label + ": " + parseFloat(series.data[XdataIndex][1]).toFixed(2) );
-        		});
-        		return "<b>%x</b> <br/>" + outputs.join("<br/>");
-        	} else return null;
-	    },
-        xDateFormat: "%m/%d %H:%M"
-    }
-};
-
-flotLineChart.bind("plothover", function (event, pos, item) {
-	if ( currentPlot ) {
-		currentPlot.getOptions().grid.markings = [{
-			xaxis: {
-				from: pos.x,
-				to: pos.x
-			},
-			color: "#ececec"
-		}];
-		currentPlot.setupGrid();
-		currentPlot.draw();
-	}
-});
-
-var propKeyMapData = null;
-
-var currentDevice, currentEnd, currentStart;
-
-function drawPlotChart(){
-	// get properties
-	var values = propertiesSelect.val();
-	var datasets = [];
-	if (values && values.length) {
-		values.forEach(function(val){
-			if (propKeyMapData[val]) datasets.push(propKeyMapData[val]);
-		});
-	}
-	if (datasets.length) {
-		currentPlot = $.plot(flotLineChart, datasets, plotOptions);
-	} else currentPlot = null;
-}
-
 function tuneChartHeight(){
-	var chartWrap = flotLineChart.parent(),
-		iboxWrap = chartWrap.parents(".ibox"),
+	var iboxWrap = chartWrap.parents(".ibox"),
 		mainWrap = iboxWrap.parents(".wrapper-content");
 
 	var iboxStyles = getComputedStyle(iboxWrap.get(0)),
@@ -127,139 +56,28 @@ function triggerLoadChartData(){
 
 	if ( !deviceName ) return;
 
-	var endISOStr = rangeDate.endDate.toISOString();
-	var startISOStr = rangeDate.startDate.toISOString();
-
-	if ( currentDevice == deviceName && currentEnd == endISOStr && currentStart == startISOStr) return;
-	else {
-		currentDevice = deviceName; currentEnd = endISOStr; currentStart = startISOStr;
-	}
-
-	var rangeMaxTime = rangeDate.endDate.toDate().getTime();
-	var rangeMinTime = rangeDate.startDate.toDate().getTime();
-	var rangeDateDiff = rangeMaxTime - rangeMinTime;
-
-	var params = {
-		end: currentEnd,
-		start: currentStart
-	};
-
 	// open loading spinner
-	flotLineChart.parents(".ibox-content").addClass("sk-loading");
+	chartWrap.parent().addClass("sk-loading");
 
-	Promise.resolve($.getJSON("/api/cells/" + deviceName + "?" + encodeParamPairs(params)))
-		.then(function(res){ 
-			// console.log(res);
-
-			res.forEach(function(item){
-				item["Time_localstamp"] = moment(item["Time_convert"]).toDate().getTime();
-			});
-
-			// find unit step
-			var unitStep = Number.MAX_SAFE_INTEGER,
-				// maxStep = 1000 * 60 * 5, // 5 mins
-				minStamp, maxStamp;
-			if ( res.length > 1 ) {
-				for(var i = 0, len = res.length - 1; i < len ; i++) {
-					var item = res[i];
-					var nextItem = res[i+1];
-
-					var diff = nextItem["Time_localstamp"] - item["Time_localstamp"];
-					if (diff > 0)
-						unitStep = Math.min( unitStep, diff );
-				}
-				minStamp = res[0]["Time_localstamp"];
-				maxStamp = res[res.length - 1]["Time_localstamp"];
-			} else {
-				minStamp = maxStamp = ( res[0] ? res[0]["Time_localstamp"] : 0 );
-			}
-
-			var slicePoints = Math.min( maxChartPoints, rangeDateDiff / unitStep );
-			if ( slicePoints == maxChartPoints ) {
-				unitStep = Math.floor( rangeDateDiff / slicePoints );
-			}
-
-			var pointStamps = [], stampTick = minStamp;
-			// fit stamps before minStamp
-			while( stampTick - rangeMinTime >= unitStep ) {
-				stampTick -= unitStep;
-				pointStamps.unshift(stampTick);
-			}
-			// fit stamps after minStamp
-			stampTick = minStamp;
-			do {
-				pointStamps.push(stampTick);
-				stampTick += unitStep;
-			} while( stampTick <= rangeMaxTime );
-
-			// init stamp to items
-			var stampToItems = {};
-			pointStamps.forEach(function(stamp){
-				stampToItems[stamp] = [];
-			});
-
-			// time alignment
-			var firstPointStamp = pointStamps[0];
-			res.forEach(function(item){
-				var stamp = item["Time_localstamp"];
-
-				// find index
-				var pointStampIdx = Math.round( Math.abs( stamp - firstPointStamp ) / unitStep );
-
-				if ( pointStampIdx >= pointStamps.length ) pointStampIdx = pointStamps.length - 1;
-
-				var stkey = pointStamps[pointStampIdx];
-				if ( stampToItems[stkey] ) stampToItems[stkey].push(item);
-			});
-
-			// propKeyMapData init
-			propKeyMapData = {};
-			$(propertiesSelect.get(0).options).each(function(idx, item){
-				var propKey = item.value;
-				propKeyMapData[propKey] = {
-					label: item.label,
-					data: []
-				};
-			});
-
-			var propKeys = Object.keys(propKeyMapData);
-
-			pointStamps.forEach(function(pointStamp){
-				var items = stampToItems[pointStamp];
-				if ( items && items.length ) {
-					propKeys.forEach(function(propKey){
-
-						var propAvg = items.map(function(item){ return item[propKey]; })
-							.reduce(function(acc, cur){ 
-								if ( isNaN(cur) ) return acc;
-								else if ( isNaN(acc) ) return cur;
-								else return acc + cur;
-							})
-							/ items.length;
-
-						propAvg = isNaN(propAvg) ? null : propAvg;
-
-						propKeyMapData[propKey].data.push( [ pointStamp, propAvg ] );
-					});
-				} else {
-					propKeys.forEach(function(propKey){
-						propKeyMapData[propKey].data.push( [ pointStamp, null ] );
-					});
-				}
-			});
-
-			drawPlotChart();
-
-			// close loading spinner
-			flotLineChart.parents(".ibox-content").removeClass("sk-loading");
-		});
+	flotLineChart.loadData({
+		deviceName: deviceName,
+		props: propertiesSelect.val(),
+		dateFrom: rangeDate.startDate,
+		dateTo: rangeDate.endDate
+	})
+	.then(function(){
+		// close loading spinner
+		chartWrap.parent().removeClass("sk-loading");
+	});
 }
 
 dateRangePicker.on('apply.daterangepicker', triggerLoadChartData);
 
 deviceSelect.change(triggerLoadChartData);
 
-propertiesSelect.change(drawPlotChart);
+propertiesSelect.change(function(){
+	flotLineChart.setPropsShow(propertiesSelect.val());
+});
 
 tuneChartHeight();
 
---
+++ assets/js/widgets/device-chart.js
@@ -0,0 +1,254 @@
+
+(function(scope){
+
+    var _assign = function(source, nextSource){
+        var to = {};
+
+        for (var key in source) {
+            to[key] = source[key];
+        }
+        
+        for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                if ( Object.prototype.toString.call(nextSource[nextKey]) == "[object Object]"
+                    && Object.prototype.toString.call(source[nextKey]) == "[object Object]" ) {
+                    to[nextKey] = _assign(source[nextKey], nextSource[nextKey]);
+                } else {
+                    to[nextKey] = nextSource[nextKey];
+                }
+            }
+        }
+        return to;
+    };
+
+    var _plotOptions = {
+        xaxis: {
+            mode: 'time',
+            timezone: "browser"
+        },
+        yaxis: {
+            max: 100
+        },
+        legend: {
+            show: true
+        },
+        colors: ["#c62828", "#283593", "#00695C", "#F9A825", "#37474F", "#6A1B9A", "#0277BD", "#558B2F", "#EF6C00", "#4E342E"],
+        grid: {
+            color: "#999999",
+            clickable: true,
+            tickColor: "#D4D4D4",
+            borderWidth:0,
+            hoverable: true //IMPORTANT! this is needed for tooltip to work,
+        },
+        tooltip: true,
+        tooltipOpts: {
+            xDateFormat: "%m/%d %H:%M"
+        }
+    };
+
+    var _defaultOptions = {
+        appendTo: document.body,
+        maxPoints: 500
+    };
+
+    function dc(options){
+        var self = this;
+
+        self._options = _assign(_defaultOptions, options || {});
+
+        self._el = $('<div class="flot-chart-content"></div>');
+
+        $(self._options.appendTo).append(self._el);
+
+        self._plotOps = _assign(_plotOptions, {
+            tooltipOpts: {
+                content: (function(ins){
+                    return function(data, x, y, dataObject) {
+                        if (!ins._plot) return null;
+                        var XdataIndex = dataObject.dataIndex;
+                        var currentAllData = ins._plot.getData();
+                        var outputs = [];
+                        currentAllData.forEach(function(series){
+                            outputs.push(series.label + ": " + parseFloat(series.data[XdataIndex][1]).toFixed(2) );
+                        });
+                        return "<b>%x</b> <br/>" + outputs.join("<br/>");
+                    };
+                })(self)
+            }
+        });
+
+        self._stampMapData = null;
+
+        self._el.bind("plothover", function (event, pos, item) {
+            if (!self._plot) return;
+            self._plot.getOptions().grid.markings = [{
+                xaxis: {
+                    from: pos.x,
+                    to: pos.x
+                },
+                color: "#ececec"
+            }];
+            self._plot.setupGrid();
+            self._plot.draw();
+        });
+
+    }
+
+    // dateFrom, dateTo, date, deviceName, props
+    dc.prototype.loadData = function(opts){
+        var self = this;
+
+        opts = _assign({
+            date: new Date()
+        }, opts || {});
+
+        if ( !opts.deviceName ) return console.error("DeviceChart: deviceName is missing.");
+
+        // reset
+        self._pointStamps = [];
+        self._stampMapData = null;
+
+        var startDate = opts.dateFrom ? moment(opts.dateFrom) : moment(opts.date).startOf('day');
+        var endDate = opts.dateTo ? moment(opts.dateTo) : moment(opts.date).endOf('day');
+
+        var maxPoints = self._options.maxPoints,
+            rangeMaxTime = endDate.toDate().getTime(),
+            rangeMinTime = startDate.toDate().getTime(),
+            rangeDateDiff = rangeMaxTime - rangeMinTime;
+
+        var params = {
+            end: endDate.toISOString(),
+            start: startDate.toISOString()
+        };
+
+        var url = "/api/cells/" + opts.deviceName + "?" + encodeParamPairs(params);
+
+        return Promise.resolve($.getJSON(url))
+            .then(function(res){
+
+                res.forEach(function(item){
+                    item["Time_localstamp"] = moment(item["Time_convert"]).toDate().getTime();
+                });
+
+                // find unit step
+                var unitStep = Number.MAX_SAFE_INTEGER,
+                    // maxStep = 1000 * 60 * 5, // 5 mins
+                    minStamp, maxStamp;
+                if ( res.length > 1 ) {
+                    for(var i = 0, len = res.length - 1; i < len ; i++) {
+                        var item = res[i];
+                        var nextItem = res[i+1];
+
+                        var diff = nextItem["Time_localstamp"] - item["Time_localstamp"];
+                        if (diff > 0)
+                            unitStep = Math.min( unitStep, diff );
+                    }
+                    minStamp = res[0]["Time_localstamp"];
+                    maxStamp = res[res.length - 1]["Time_localstamp"];
+                } else {
+                    minStamp = maxStamp = ( res[0] ? res[0]["Time_localstamp"] : 0 );
+                }
+
+                var slicePoints = Math.min( maxPoints, rangeDateDiff / unitStep );
+                if ( slicePoints == maxPoints ) {
+                    unitStep = Math.floor( rangeDateDiff / slicePoints );
+                }
+
+                var pointStamps = [], stampTick = minStamp;
+                // fit stamps before minStamp
+                while( stampTick - rangeMinTime >= unitStep ) {
+                    stampTick -= unitStep;
+                    pointStamps.unshift(stampTick);
+                }
+                // fit stamps after minStamp
+                stampTick = minStamp;
+                do {
+                    pointStamps.push(stampTick);
+                    stampTick += unitStep;
+                } while( stampTick <= rangeMaxTime );
+
+                // init stamp to items
+                var stampToItems = {};
+                pointStamps.forEach(function(stamp){
+                    stampToItems[stamp] = [];
+                });
+
+                // time alignment
+                var firstPointStamp = pointStamps[0];
+                res.forEach(function(item){
+                    var stamp = item["Time_localstamp"];
+
+                    // find index
+                    var pointStampIdx = Math.round( Math.abs( stamp - firstPointStamp ) / unitStep );
+
+                    if ( pointStampIdx >= pointStamps.length ) pointStampIdx = pointStamps.length - 1;
+
+                    var stkey = pointStamps[pointStampIdx];
+                    if ( stampToItems[stkey] ) stampToItems[stkey].push(item);
+                });
+
+                self._pointStamps = pointStamps;
+                self._stampMapData = stampToItems;
+
+                self.setPropsShow(opts.props || []);
+
+                return res;
+            });
+    };
+
+    dc.prototype.setPropsShow = function(props){
+        var self = this;
+        if ( !self._stampMapData || self._pointStamps.length == 0 ) return;
+
+        var datasets = [];
+        var propKeyMapData = {};
+
+        props.forEach(function(item){
+            var isStr = typeof item == "string",
+                propKey = isStr ? item : item.value,
+                propLabel = isStr ? item : item.label;
+            propKeyMapData[propKey] = {
+                label: propLabel,
+                data: []
+            };
+        });
+        
+        var propKeys = Object.keys(propKeyMapData);
+
+        self._pointStamps.forEach(function(pointStamp){
+            var items = self._stampMapData[pointStamp];
+            if ( items && items.length ) {
+                propKeys.forEach(function(propKey){
+
+                    var propAvg = items.map(function(item){ return item[propKey]; })
+                        .reduce(function(acc, cur){ 
+                            if ( isNaN(cur) ) return acc;
+                            else if ( isNaN(acc) ) return cur;
+                            else return acc + cur;
+                        })
+                        / items.length;
+
+                    propAvg = isNaN(propAvg) ? null : propAvg;
+
+                    propKeyMapData[propKey].data.push( [ pointStamp, propAvg ] );
+                });
+            } else {
+                propKeys.forEach(function(propKey){
+                    propKeyMapData[propKey].data.push( [ pointStamp, null ] );
+                });
+            }
+        });
+
+        // var values = propertiesSelect.val();
+        props.forEach(function(item){
+            var isStr = typeof item == "string",
+                propKey = isStr ? item : item.value;
+            if (propKeyMapData[propKey]) datasets.push(propKeyMapData[propKey]);
+        });
+
+        self._plot = $.plot(self._el, datasets, self._plotOps);
+    };
+
+    scope.DeviceChart = dc;
+})(window);
