4729518519356f205fda5cd93abb127c5b0b67d7
KinoLien <miecowbai@gmail.com>
2020-10-26 19:45:08 +0800

    Auto set Time if it is undefined.

--- server/config/routes.js
+++ server/config/routes.js
@@ -65,6 +65,11 @@ module.exports = function(app, passport) {
             }
         } );
 
+        // time case
+        if (typeof data["Time"] === "undefined") {
+            data["Time"] = utils.toFridayFormat(new Date());
+        }
+
         if (cellId) {
             interface.addData(cellId, data)
                 .then( () => { 
