dbbca9d33f387d73246d5fc343d3ed871f5e84ce
KinoLien <miecowbai@gmail.com>
2020-10-28 18:45:13 +0800

    Force to use the service account json.

--- server/services/data-interface.js
+++ server/services/data-interface.js
@@ -2,19 +2,11 @@ var admin = require("firebase-admin");
 var utils = require('./utils');
 var md5 = require('md5');
 
-var isProduction = process.env.NODE_ENV === 'production';
+var serviceAccount = require("../config/serviceAccountKey.json");
 
-if ( isProduction ) {
-	admin.initializeApp({
-		credential: admin.credential.applicationDefault()
-	});
-} else {
-	var serviceAccount = require("../config/serviceAccountKey.json");
-
-	admin.initializeApp({
-	  credential: admin.credential.cert(serviceAccount)
-	});
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 var db = admin.firestore();
 
